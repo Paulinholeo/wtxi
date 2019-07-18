2b3d94bfede4105a77929fd116fe6e61e3d72b44
g-barbosa <giovanne.bbarbosa@gmail.com>
2019-07-18 09:16:58 -0300

    configurações monitor txi

--- cfg.py
+++ cfg.py
@@ -5,12 +5,15 @@
 import logging
 
 __author__ = "Paulo/Giovanne"
-__copyright__ = "Copyright 2019, Brascontrol"
+__copyright__ = "Copyright 2018, Brascontrol"
 __status__ = "Development"
 
 LOG_PATH = '/var/log/wtxi/'
-LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.INFO
 LOG_MAX_BYTES = 40 * (2 ** 20)
 LOG_COUNT = 25
 
+#TEMPOS DE MONITORAMENTO DOS SERVIÇOS
 
+TEMPO_MAX_TXI=10 #Em segundos
+TEMPO_MAX_BRICAP=30 #Em segundos
