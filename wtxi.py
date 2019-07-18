681b7d58bd546291d05ba1d93f463b48bd3ccf86
g-barbosa <giovanne.bbarbosa@gmail.com>
2019-07-18 09:26:04 -0300

    monitor txi

--- wtxi.py
+++ wtxi.py
@@ -4,11 +4,7 @@ import threading
 import datetime
 import psutil
 
-<<<<<<< HEAD
-from VerifyBri import *
-=======
 from check_bricap import *
->>>>>>> desenvolvimento
 
 import cfg 
 from logger import logger
@@ -17,10 +13,6 @@ __author__ = "Paulo/Giovanne"
 __copyright__ = "Copyright 2019, Brascontrol"
 __status__ = "Development"
 
-<<<<<<< HEAD
-TEMPO_MAX = 10 #Tempo em segundos
-=======
->>>>>>> desenvolvimento
 
 #Cria thread
 class MainClass(threading.Thread):
@@ -73,20 +65,11 @@ def main():
     verificaBri.start()
     verificaTxi.start()
     while True:
-<<<<<<< HEAD
-        ctempo= str(datetime.timedelta(seconds=TEMPO_MAX))    
-        tempo = datetime.datetime.now()
-        logger.info('\033[36m'+str(tempo)+' >>>> Vericando a cada ' + ctempo +'\033[0;0m\n')
-        teste = verificaTempo()
-        if not teste:
-            logger.info('\n\033[36m'+str(tempo)+' >>>> REINICIANDO TXI (pid=%d)'+'\033[0;0m\n', os.getpid() )
-=======
         ctempo= str(datetime.timedelta(seconds=cfg.TEMPO_MAX_TXI))    
         logger.info('  >>>>  Vericando a cada ' + ctempo )
         teste = verificaTempo()
         if not teste:
             logger.info('  >>>>  REINICIANDO TXI (pid=%d)\n', os.getpid() )
->>>>>>> desenvolvimento
             os.system('killall txi')
         
 if __name__ == "__main__":
